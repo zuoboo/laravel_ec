1ded83fc2f432db6d750a9675699cc34f57b2292
github zuoboo <nsnxn1022@gmail.com>
2022-09-02 06:32:43 +0900

    リソースコントローラー

--- routes/admin.php
+++ routes/admin.php
@@ -9,8 +9,7 @@ use App\Http\Controllers\Admin\Auth\NewPasswordController;
 use App\Http\Controllers\Admin\Auth\PasswordResetLinkController;
 use App\Http\Controllers\Admin\Auth\RegisteredUserController;
 use App\Http\Controllers\Admin\Auth\VerifyEmailController;
-
-
+use App\Http\Controllers\Admin\OwnersController;
 
 /*
 |--------------------------------------------------------------------------
@@ -26,6 +25,8 @@ use App\Http\Controllers\Admin\Auth\VerifyEmailController;
 Route::get('/', function () {
     return view('admin.welcome');
 });
+
+Route::resource('owners', OwnersController::class)->middleware('auth:admin');
 
 Route::get('/dashboard', function () {
     return view('admin.dashboard');
