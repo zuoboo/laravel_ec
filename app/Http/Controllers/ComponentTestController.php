6ff5ffadcc0ee1246b0e4f722c83299adb3bc604
github zuoboo <nsnxn1022@gmail.com>
2022-09-01 12:53:46 +0900

    変数

--- app/Http/Controllers/ComponentTestController.php
+++ app/Http/Controllers/ComponentTestController.php
@@ -6,11 +6,13 @@ use Illuminate\Http\Request;
 
 class ComponentTestController extends Controller
 {
-    public function showComponent1(){
-        return view('tests.component-test1');
+    public function showComponent1()
+    {
+        $message = 'メッセージ';
+        return view('tests.component-test1', compact('message'));
     }
-    public function showComponent2(){
+    public function showComponent2()
+    {
         return view('tests.component-test2');
     }
-
 }
--- resources/views/tests/component-test1.blade.php
+++ resources/views/tests/component-test1.blade.php
@@ -2,5 +2,5 @@
     <x-slot name="header">ヘッダー1</x-slot>
     コンポーネントテスト1
 
-    <x-tests.card title="タイトル" content="本文" />
+    <x-tests.card title="タイトル" content="本文" :message="$message" />
 </x-tests.app>
