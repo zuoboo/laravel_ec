bb8c1a8e0d8f3180ad944fe15fd935ceb17da3b0
github zuoboo <nsnxn1022@gmail.com>
2022-09-01 14:25:54 +0900

    classBase

---
+++ resources/views/components/tests/test-class-base-component.blade.php
@@ -0,0 +1,6 @@
+<div>
+    クラスベースのコンポーネントです
+    <div>{{ $classBaseMessage }}</div>
+    <div>{{ $defaultMessage }}</div>
+    <!-- Let all your things have their places; let each part of your business have its time. - Benjamin Franklin -->
+</div>
