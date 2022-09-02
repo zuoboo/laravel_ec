1158f845339e2a4626756a4c855937cd1deea922
github zuoboo <nsnxn1022@gmail.com>
2022-09-02 14:40:19 +0900

    index, create

--- app/Http/Controllers/Admin/OwnersController.php
+++ app/Http/Controllers/Admin/OwnersController.php
@@ -20,19 +20,22 @@ class OwnersController extends Controller
 
     public function index()
     {
-        $date_now = Carbon::now();
-        $date_parse = Carbon::parse(now());
-        echo $date_now;
-        echo $date_parse;
-        $e_all = Owner::all();
-        $q_get = DB::table('owners')->select('name','created_at')->get();
+        // $date_now = Carbon::now();
+        // $date_parse = Carbon::parse(now());
+        // echo $date_now;
+        // echo $date_parse;
+        // $e_all = Owner::all();
+        // $q_get = DB::table('owners')->select('name','created_at')->get();
         // $q_first = DB::table('owners')->select('name')->first();
 
         // $c_test = collect([
         //     'name' => 'テスト'
         // ]);
         // dd($e_all, $q_get, $q_first, $c_test);
-        return view('admin.owners.index', compact('e_all', 'q_get'));
+        $owners = Owner::select('name', 'email', 'created_at')->get();
+
+
+        return view('admin.owners.index', compact('owners'));
     }
 
     /**
@@ -42,7 +45,7 @@ class OwnersController extends Controller
      */
     public function create()
     {
-        //
+        return view('admin.owners.create');
     }
 
     /**
