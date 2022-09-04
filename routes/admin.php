5db7750af94631e6514c07cf865401e87a9f22f2
github zuoboo <nsnxn1022@gmail.com>
2022-09-04 14:19:29 +0900

    ルート、ビュー編集

--- routes/admin.php
+++ routes/admin.php
@@ -22,11 +22,11 @@ use App\Http\Controllers\Admin\OwnersController;
 |
 */
 
-Route::get('/', function () {
-    return view('admin.welcome');
-});
+// Route::get('/', function () {
+//     return view('admin.welcome');
+// });
 
-Route::resource('owners', OwnersController::class)->middleware('auth:admin');
+Route::resource('owners', OwnersController::class)->middleware('auth:admin')->except(['show']);
 
 Route::prefix('expired-owners')-> middleware('auth:admin')->group(function(){
     Route::get('index', [OwnersController::class, 'expiredOwnerIndex'])->name('expired-owners.index');
