3a94116551dd54b04f6f27dc79cf1a62db9aeab3
github zuoboo <nsnxn1022@gmail.com>
2022-09-04 20:45:27 +0900

    shopCreateDelete

--- resources/views/admin/owners/edit.blade.php
+++ resources/views/admin/owners/edit.blade.php
@@ -17,7 +17,8 @@
                             </div>
                             <div class="lg:w-1/2 md:w-2/3 mx-auto">
                                 <x-auth-validation-errors class="mb-4" :errors="$errors" />
-                                <form method="POST" action="{{ route('admin.owners.update', ['owner' => $owner->id]) }}">
+                                <form method="POST"
+                                    action="{{ route('admin.owners.update', ['owner' => $owner->id]) }}">
                                     @method('PUT')
                                     @csrf
                                     <div class="-m-2">
@@ -34,8 +35,18 @@
                                             <div class="relative">
                                                 <label for="email"
                                                     class="leading-7 text-sm text-gray-600">メールアドレス</label>
-                                                <input type="email" id="email" name="email" value="{{ $owner->email }}" required
+                                                <input type="email" id="email" name="email"
+                                                    value="{{ $owner->email }}" required
                                                     class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-blue-500 focus:bg-white focus:ring-2 focus:ring-blue-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+                                            </div>
+                                        </div>
+                                        <div class="p-2 w-1/2 mx-auto">
+                                            <div class="relative">
+                                                <label for="shop"
+                                                    class="leading-7 text-sm text-gray-600">店名</label>
+                                                <div
+                                                    class="w-full bg-gray-100 bg-opacity-50 rounded focus:bg-white focus:ring-2 focus:ring-blue-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+                                                    {{ $owner->shop->name }}</div>
                                             </div>
                                         </div>
                                         <div class="p-2 w-1/2 mx-auto">
@@ -56,10 +67,10 @@
                                             </div>
                                         </div>
                                         <div class="p-2 w-full  flex justify-around mt-4">
-                                            <button
-                                                type="button" onclick="location.href='{{ route('admin.owners.index') }}'"class="bg-gray-200 border-0 py-2 px-8 focus:outline-none hover:bg-gray-400 rounded text-lg">戻る</button>
-                                            <button
-                                                type="submit" class="text-white bg-blue-500 border-0 py-2 px-8 focus:outline-none hover:bg-blue-600 rounded text-lg">更新する</button>
+                                            <button type="button"
+                                                onclick="location.href='{{ route('admin.owners.index') }}'"class="bg-gray-200 border-0 py-2 px-8 focus:outline-none hover:bg-gray-400 rounded text-lg">戻る</button>
+                                            <button type="submit"
+                                                class="text-white bg-blue-500 border-0 py-2 px-8 focus:outline-none hover:bg-blue-600 rounded text-lg">更新する</button>
                                         </div>
                                     </div>
                                 </form>
