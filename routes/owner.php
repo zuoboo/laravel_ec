a020225f680153462caf0b16f360ec20a2f08783
github zuoboo <nsnxn1022@gmail.com>
2022-09-05 10:52:55 +0900

    middleware, abort(404)

--- routes/owner.php
+++ routes/owner.php
@@ -9,6 +9,7 @@ use App\Http\Controllers\Owner\Auth\NewPasswordController;
 use App\Http\Controllers\Owner\Auth\PasswordResetLinkController;
 use App\Http\Controllers\Owner\Auth\RegisteredUserController;
 use App\Http\Controllers\Owner\Auth\VerifyEmailController;
+use App\Http\Controllers\Owner\ShopController;
 
 
 
@@ -25,6 +26,12 @@ use App\Http\Controllers\Owner\Auth\VerifyEmailController;
 
 Route::get('/', function () {
     return view('owner.welcome');
+});
+
+Route::prefix('shops')-> middleware('auth:owners')->group(function(){
+    Route::get('index', [ShopController::class, 'index'])->name('shops.index');
+    Route::get('edit/{shop}', [ShopController::class, 'edit'])->name('shops.edit');
+    Route::get('update/{shop}', [ShopController::class, 'update'])->name('shops.update');
 });
 
 Route::get('/dashboard', function () {
