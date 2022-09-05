b0fcdf8cf3e81ad910d69727a83494177ac6fa89
github zuoboo <nsnxn1022@gmail.com>
2022-09-05 17:24:32 +0900

    shop edit,update

--- resources/views/owner/shops/index.blade.php
+++ resources/views/owner/shops/index.blade.php
@@ -9,28 +9,23 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
+                    <x-flash-message status="session('status')" />
                     @foreach ($shops as $shop)
-                    <div class="w-1/2 p-4">
-                        <a href="{{ route('owner.shops.edit', ['shop' => $shop->id]) }}">
-                            <div class="border rounded-md p-4">
-                                <div class="mb-4">
-                                    @if ($shop->is_selling)
-                                        <span class="border p-2 rounded-md bg-blue-400 text-white">販売中</span>
-                                    @else
-                                        <span class="border p-2 rounded-md bg-red-400 text-white">停止中</span>
-                                    @endif
+                        <div class="w-1/2 p-4">
+                            <a href="{{ route('owner.shops.edit', ['shop' => $shop->id]) }}">
+                                <div class="border rounded-md p-4">
+                                    <div class="mb-4">
+                                        @if ($shop->is_selling)
+                                            <span class="border p-2 rounded-md bg-blue-400 text-white">販売中</span>
+                                        @else
+                                            <span class="border p-2 rounded-md bg-red-400 text-white">停止中</span>
+                                        @endif
+                                    </div>
+                                    <div class="text-xl">{{ $shop->name }}</div>
+                                    <x-shop-thumbnail :filename="$shop->filename" />
                                 </div>
-                                <div class="text-xl">{{ $shop->name }}</div>
-                                <div>
-                                    @if (empty($shop->filename))
-                                        <img src="{{ asset('images/no_image.jpg') }}">
-                                    @else
-                                        <img src="{{ asset('storage/shops/' . $shop->filename) }}">
-                                    @endif
-                                </div>
-                            </div>
-                        </a>
-                    </div>
+                            </a>
+                        </div>
                     @endforeach
                 </div>
             </div>
