ee30de5d594fcef663fe0674c7c4b5f0e0c8effa
github zuoboo <nsnxn1022@gmail.com>
2022-09-05 16:11:13 +0900

    サービスへの切り離し

--- app/Http/Controllers/Owner/ShopController.php
+++ app/Http/Controllers/Owner/ShopController.php
@@ -9,6 +9,7 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Storage;
 use InterventionImage;
 use App\Http\Requests\UploadImageRequest;
+use App\Services\ImageService;
 
 class ShopController extends Controller
 {
@@ -56,15 +57,9 @@ class ShopController extends Controller
 
         $imageFile = $request->image; //一時保存
         if (!is_null($imageFile) && $imageFile->isValid()) {
-            // Storage::putFile('public/shops', $imageFile); リサイズなしの場合
 
-            $fileName = uniqid(rand() . '_');
-            $extension = $imageFile->extension();
-            $fileNameToStore = $fileName . '.' . $extension;
-            $resizedImage = InterventionImage::make($imageFile)->resize(1920, 1080)->encode();
+            $fileNameToStore = ImageService::upload($imageFile, 'shops');
 
-            // dd($imageFile, $resizedImage);
-            Storage::put('public/shops/' . $fileNameToStore, $resizedImage);
         }
 
         return redirect()->route('owner.shops.index');
