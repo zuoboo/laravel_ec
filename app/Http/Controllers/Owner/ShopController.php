cdab50778c16e31c9ff3877ebe4c0a0c289372fd
github zuoboo <nsnxn1022@gmail.com>
2022-09-05 15:34:00 +0900

    intervention/image

--- app/Http/Controllers/Owner/ShopController.php
+++ app/Http/Controllers/Owner/ShopController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use App\Models\Shop;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Storage;
+use InterventionImage;
 
 class ShopController extends Controller
 {
@@ -34,6 +35,8 @@ class ShopController extends Controller
 
     public function index()
     {
+
+
         //$ownerId = Auth::id();
         $shops = Shop::where('owner_id', Auth::id())->get();
 
@@ -51,8 +54,16 @@ class ShopController extends Controller
     {
 
         $imageFile = $request->image; //一時保存
-        if(!is_null($imageFile) && $imageFile->isValid() ){
-            Storage::putFile('public/shops', $imageFile);
+        if (!is_null($imageFile) && $imageFile->isValid()) {
+            // Storage::putFile('public/shops', $imageFile); リサイズなしの場合
+
+            $fileName = uniqid(rand() . '_');
+            $extension = $imageFile->extension();
+            $fileNameToStore = $fileName . '.' . $extension;
+            $resizedImage = InterventionImage::make($imageFile)->resize(1920, 1080)->encode();
+
+            // dd($imageFile, $resizedImage);
+            Storage::put('public/shops/' . $fileNameToStore, $resizedImage);
         }
 
         return redirect()->route('owner.shops.index');
