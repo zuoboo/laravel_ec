aac9224801eb1c6304c82b6f089e16ba06350bc2
github zuoboo <nsnxn1022@gmail.com>
2022-09-05 21:23:04 +0900

    image index

--- app/Http/Controllers/Owner/ImageController.php
+++ app/Http/Controllers/Owner/ImageController.php
@@ -4,8 +4,38 @@ namespace App\Http\Controllers\Owner;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Image;
+use Illuminate\Support\Facades\Auth;
+
 
 class ImageController extends Controller
 {
-    //
-}
+    public function __construct()
+    {
+        $this->middleware('auth:owners');
+
+        $this->middleware(function ($request, $next) {
+
+            $id = $request->route()->parameter('image'); //imageのid取得
+            if (!is_null($id)) { // null判定
+                $imagesOwnerId = Image::findOrFail($id)->owner->id;
+                $imageId = (int)$imagesOwnerId; // キャスト 文字列→数値に型変換 $imageId = Auth::id();
+                if ($imageId !== Auth::id()) { // 同じでなかったら
+                    abort(404); // 404画面表示 }
+                }
+            }
+            return $next($request);
+        });
+    }
+
+    public function index()
+    {
+        $images = Image::where('owner_id', Auth::id())
+        ->orderBy('updated_at', 'desc')
+        ->paginate(20);
+
+        return view('owner.images.index', compact('images'));
+    }
+    }
+
+
--- app/Models/Owner.php
+++ app/Models/Owner.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Database\Eloquent\Model;
 use App\Models\Shop;
+use App\Models\Image;
 
 class Owner extends Authenticatable
 {
@@ -45,5 +46,9 @@ class Owner extends Authenticatable
     public function shop()
     {
         return $this->hasOne(Shop::class);
+    }
+    public function image()
+    {
+        return $this->hasMany(Image::class);
     }
 }
