192aebeb23067f2186317c1691fcbd39dec9d05a
github zuoboo <nsnxn1022@gmail.com>
2022-09-06 15:13:37 +0900

    stock

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use App\Models\Shop;
+use App\Models\Stock;
 use App\Models\SecondaryCategory;
 use App\Models\Image;
 
@@ -23,6 +24,10 @@ class Product extends Model
     public function imageFirst()
     {
         return $this->belongsTo(Image::class, 'image1', 'id');
+    }
+    public function stock()
+    {
+        return $this->hasMany(Stock::class);
     }
 
 }
