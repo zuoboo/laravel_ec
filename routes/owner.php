cd8e76a1dd3bcd777d75d132c35b83fc395b4d7e
github zuoboo <nsnxn1022@gmail.com>
2022-09-06 13:56:18 +0900

    product初期設定

--- routes/owner.php
+++ routes/owner.php
@@ -11,6 +11,7 @@ use App\Http\Controllers\Owner\Auth\RegisteredUserController;
 use App\Http\Controllers\Owner\Auth\VerifyEmailController;
 use App\Http\Controllers\Owner\ShopController;
 use App\Http\Controllers\Owner\ImageController;
+use App\Http\Controllers\Owner\ProductController;
 
 
 
@@ -36,6 +37,7 @@ Route::prefix('shops')-> middleware('auth:owners')->group(function(){
 });
 
 Route::resource('images', ImageController::class)->middleware('auth:owners')->except(['show']);
+Route::resource('products', ProductController::class)->middleware('auth:owners')->except(['show']);
 
 Route::get('/dashboard', function () {
     return view('owner.dashboard');
