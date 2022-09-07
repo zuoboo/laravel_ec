82b4d246f511b90aee891dc69241f1f36172aa98
github zuoboo <nsnxn1022@gmail.com>
2022-09-07 17:05:16 +0900

    product destroy

--- resources/views/owner/products/edit.blade.php
+++ resources/views/owner/products/edit.blade.php
@@ -51,83 +51,93 @@
                                     <label for="current_quantity" class="leading-7 text-sm text-gray-600">現在の在庫</label>
                                     <input type="hidden" id="current_quantity" name="current_quantity"
                                         value="{{ $quantity }}">
-                                    <div class="w-full bg-gray-100 bg-opacity-50 rounded text-base outline-none text-gray-700 py-1 px-3 leading-8">{{ $quantity }}</div>
+                                    <div
+                                        class="w-full bg-gray-100 bg-opacity-50 rounded text-base outline-none text-gray-700 py-1 px-3 leading-8">
+                                        {{ $quantity }}</div>
                                 </div>
                             </div>
-                                <div class="p-2 w-1/2 mx-auto">
-                                    <div class="relative flex justify-around">
-                                        <div><input type="radio" name="type" value="1" class="mr-2" checked>追加</div>
-                                        <div><input type="radio" name="type" value="2" class="mr-2">削減</div>
-                                    </div>
-                                </div>
-                                <div class="p-2 w-1/2 mx-auto">
-                                    <div class="relative">
-                                        <label for="quantity" class="leading-7 text-sm text-gray-600">数量 ※必須</label>
-                                        <input type="number" id="quantity" name="quantity" value="0" required
-                                            class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-blue-500 focus:bg-white focus:ring-2 focus:ring-blue-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
-                                        <span class="text-sm">0〜99の範囲で入力してください</span>
-                                    </div>
-                                </div>
-
-                                <div class="p-2 w-1/2 mx-auto">
-                                    <div class="relative">
-                                        <label for="shop_id" class="leading-7 text-sm text-gray-600">販売する店舗</label>
-                                        <select name="shop_id" id="shop_id"
-                                            class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-blue-500 focus:bg-white focus:ring-2 focus:ring-blue-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
-                                            @foreach ($shops as $shop)
-                                                <option value="{{ $shop->id }}"
-                                                    @if ($shop->id === $product->shop_id) selected @endif>
-                                                    {{ $shop->name }}
-                                                </option>
-                                            @endforeach
-                                        </select>
-                                    </div>
-                                </div>
-                                <div class="p-2 w-1/2 mx-auto">
-                                    <div class="relative">
-                                        <label for="category" class="leading-7 text-sm text-gray-600">カテゴリー</label>
-                                        <select name="category" id="category"
-                                            class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-blue-500 focus:bg-white focus:ring-2 focus:ring-blue-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
-                                            @foreach ($categories as $category)
-                                                <optgroup label="{{ $category->name }}">
-                                                    @foreach ($category->secondary as $secondary)
-                                                        <option value="{{ $secondary->id }}"
-                                                            @if ($secondary->id === $product->secondary_category_id) selected @endif>
-                                                            {{ $secondary->name }}
-                                                        </option>
-                                                    @endforeach
-                                            @endforeach
-                                        </select>
-                                    </div>
-                                </div>
-                            </div>
-                            <x-select-image :images="$images" currentId="{{ $product->image1 }}"
-                                currentImage="{{ $product->imageFirst->filename ?? '' }}" name="image1" />
-                            <x-select-image :images="$images" currentId="{{ $product->image2 }}"
-                                currentImage="{{ $product->imageSecond->filename ?? '' }}" name="image2" />
-
-                            <x-select-image :images="$images" currentId="{{ $product->image3 }}"
-                                currentImage="{{ $product->imageThird->filename ?? '' }}" name="image3" />
-
-                            <x-select-image :images="$images" currentId="{{ $product->image4 }}"
-                                currentImage="{{ $product->imageFourth->filename ?? '' }}" name="image4" />
-
                             <div class="p-2 w-1/2 mx-auto">
                                 <div class="relative flex justify-around">
-                                    <div><input type="radio" name="is_selling" value="1" class="mr-2"
-                                            @if ($product->is_selling === 1) { checked } @endif>販売中</div>
-                                    <div><input type="radio" name="is_selling" value="0" class="mr-2"
-                                            @if ($product->is_selling === 0) { checked } @endif>停止中</div>
+                                    <div><input type="radio" name="type" value="1" class="mr-2" checked>追加
+                                    </div>
+                                    <div><input type="radio" name="type" value="2" class="mr-2">削減</div>
                                 </div>
                             </div>
-                            <div class="p-2 w-full  flex justify-around mt-4">
-                                <button type="button"
-                                    onclick="location.href='{{ route('owner.products.index') }}'"class="bg-gray-200 border-0 py-2 px-8 focus:outline-none hover:bg-gray-400 rounded text-lg">戻る</button>
-                                <button type="submit"
-                                    class="text-white bg-blue-500 border-0 py-2 px-8 focus:outline-none hover:bg-blue-600 rounded text-lg">更新する</button>
+                            <div class="p-2 w-1/2 mx-auto">
+                                <div class="relative">
+                                    <label for="quantity" class="leading-7 text-sm text-gray-600">数量 ※必須</label>
+                                    <input type="number" id="quantity" name="quantity" value="0" required
+                                        class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-blue-500 focus:bg-white focus:ring-2 focus:ring-blue-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+                                    <span class="text-sm">0〜99の範囲で入力してください</span>
+                                </div>
                             </div>
 
+                            <div class="p-2 w-1/2 mx-auto">
+                                <div class="relative">
+                                    <label for="shop_id" class="leading-7 text-sm text-gray-600">販売する店舗</label>
+                                    <select name="shop_id" id="shop_id"
+                                        class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-blue-500 focus:bg-white focus:ring-2 focus:ring-blue-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+                                        @foreach ($shops as $shop)
+                                            <option value="{{ $shop->id }}"
+                                                @if ($shop->id === $product->shop_id) selected @endif>
+                                                {{ $shop->name }}
+                                            </option>
+                                        @endforeach
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="p-2 w-1/2 mx-auto">
+                                <div class="relative">
+                                    <label for="category" class="leading-7 text-sm text-gray-600">カテゴリー</label>
+                                    <select name="category" id="category"
+                                        class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-blue-500 focus:bg-white focus:ring-2 focus:ring-blue-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+                                        @foreach ($categories as $category)
+                                            <optgroup label="{{ $category->name }}">
+                                                @foreach ($category->secondary as $secondary)
+                                                    <option value="{{ $secondary->id }}"
+                                                        @if ($secondary->id === $product->secondary_category_id) selected @endif>
+                                                        {{ $secondary->name }}
+                                                    </option>
+                                                @endforeach
+                                        @endforeach
+                                    </select>
+                                </div>
+                            </div>
+                        </div>
+                        <x-select-image :images="$images" currentId="{{ $product->image1 }}"
+                            currentImage="{{ $product->imageFirst->filename ?? '' }}" name="image1" />
+                        <x-select-image :images="$images" currentId="{{ $product->image2 }}"
+                            currentImage="{{ $product->imageSecond->filename ?? '' }}" name="image2" />
 
+                        <x-select-image :images="$images" currentId="{{ $product->image3 }}"
+                            currentImage="{{ $product->imageThird->filename ?? '' }}" name="image3" />
+
+                        <x-select-image :images="$images" currentId="{{ $product->image4 }}"
+                            currentImage="{{ $product->imageFourth->filename ?? '' }}" name="image4" />
+
+                        <div class="p-2 w-1/2 mx-auto">
+                            <div class="relative flex justify-around">
+                                <div><input type="radio" name="is_selling" value="1" class="mr-2"
+                                        @if ($product->is_selling === 1) { checked } @endif>販売中</div>
+                                <div><input type="radio" name="is_selling" value="0" class="mr-2"
+                                        @if ($product->is_selling === 0) { checked } @endif>停止中</div>
+                            </div>
+                        </div>
+                        <div class="p-2 w-full  flex justify-around mt-4">
+                            <button type="button"
+                                onclick="location.href='{{ route('owner.products.index') }}'"class="bg-gray-200 border-0 py-2 px-8 focus:outline-none hover:bg-gray-400 rounded text-lg">戻る</button>
+                            <button type="submit"
+                                class="text-white bg-blue-500 border-0 py-2 px-8 focus:outline-none hover:bg-blue-600 rounded text-lg">更新する</button>
+                        </div>
+                    </form>
+                    <form id="delete_{{ $product->id }}" method="post"
+                        action="{{ route('owner.products.destroy', ['product' => $product->id]) }}">
+                        @csrf
+                        @method('DELETE')
+                        <div class="p-2 w-full  flex justify-around mt-32">
+                            <a href="#" data-id="{{ $product->id }}" onclick="deletePost(this)"
+                                class="text-white bg-red-400 border-0 py-2 px-4 focus:outline-none hover:bg-red-500 rounded ">削除する</a>
+                        </div>
                     </form>
                 </div>
             </div>
@@ -150,5 +160,12 @@
                 // MicroModal.close(modal); //モーダルを閉じる
             }, )
         })
+
+        function deletePost(e) {
+            'use strict';
+            if (confirm('本当に削除してもいいですか?')) {
+                document.getElementById('delete_' + e.dataset.id).submit();
+            }
+        }
     </script>
 </x-app-layout>
