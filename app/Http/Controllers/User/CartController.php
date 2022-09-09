615d0a1eb3381c0946c39441f58e9bcbfbb19275
github zuoboo <nsnxn1022@gmail.com>
2022-09-09 09:56:18 +0900

    cart index

--- app/Http/Controllers/User/CartController.php
+++ app/Http/Controllers/User/CartController.php
@@ -5,10 +5,28 @@ namespace App\Http\Controllers\User;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Cart;
+use App\Models\User;
 use Illuminate\Support\Facades\Auth;
 
 class CartController extends Controller
 {
+    public function index()
+    {
+        $user = User::findOrFail(Auth::id());
+        $products = $user->products;
+        $totalPrice = 0;
+
+        foreach($products as $product)
+        {
+            $totalPrice += $product->price * $product->pivot->quantity;
+        }
+
+        // dd($products, $totalPrice);
+
+        return view('user.cart', compact('products', 'totalPrice'));
+    }
+
+
     public function add(Request $request)
     {
         $itemInCart = Cart::where('product_id', $request->product_id)
@@ -25,6 +43,6 @@ class CartController extends Controller
                 'quantity' => $request->quantity
             ]);
         }
-        dd('テスト');
+        return redirect()->route('user.cart.index');
     }
 }
