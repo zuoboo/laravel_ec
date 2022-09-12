5ebf75f33e82f76476943663b6ac7fa96801f3aa
github zuoboo <nsnxn1022@gmail.com>
2022-09-12 09:17:29 +0900

    検索フォーム キーワード

--- app/Http/Controllers/User/ItemController.php
+++ app/Http/Controllers/User/ItemController.php
@@ -38,6 +38,7 @@ class ItemController extends Controller
 
         $products = Product::availableItems()
         ->selectCategory($request->category ?? '0')
+        ->searchKeyword($request->keyword)
         ->sortOrder($request->sort)
         ->paginate($request->pagination ?? '20');
 
